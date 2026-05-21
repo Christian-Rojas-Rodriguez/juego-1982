215b1f2b500ca1b1f02e3f18fdbcdf7f4c7810ae
Matías Vergara <mavergaravicencio@estudiantes.unsam.edu.ar>
2026-05-21 13:50:24 -0300

    feat: añadir sprite del jugador

--- src/main/java/game/entities/player/Player.java
+++ src/main/java/game/entities/player/Player.java
@@ -2,13 +2,20 @@ package game.entities.player;
 
 import game.entities.Nave;
 import processing.core.PApplet;
+import processing.core.PImage;
 
 public class Player extends Nave {
 
+    private static final String SPRITE_PATH = "assets/images/mirage/mirage.png";
+    private static final float SPRITE_ANCHO = 60;
+    private static final float SPRITE_ALTO = 60;
+
     private boolean izquierda, derecha, arriba, abajo;
+    private final PImage sprite;
 
     public Player(PApplet sketch) {
         super(sketch, sketch.width / 2f, sketch.height - 100, 4, 100);
+        sprite = sketch.loadImage(SPRITE_PATH);
     }
 
     @Override
@@ -18,12 +25,18 @@ public class Player extends Nave {
         if (arriba)    y -= velocidad;
         if (abajo)     y += velocidad;
 
-        x = PApplet.constrain(x, 15, sketch.width  - 15);
-        y = PApplet.constrain(y, 20, sketch.height - 15);
+        x = PApplet.constrain(x, SPRITE_ANCHO / 2, sketch.width  - SPRITE_ANCHO / 2);
+        y = PApplet.constrain(y, SPRITE_ALTO / 2, sketch.height - SPRITE_ALTO / 2);
     }
 
     @Override
     public void render() {
+        if (sprite != null) {
+            sketch.imageMode(PApplet.CENTER);
+            sketch.image(sprite, x, y, SPRITE_ANCHO, SPRITE_ALTO);
+            return;
+        }
+
         sketch.noStroke();
         sketch.fill(0, 200, 255);
         sketch.triangle(x, y - 20, x - 15, y + 15, x + 15, y + 15);
